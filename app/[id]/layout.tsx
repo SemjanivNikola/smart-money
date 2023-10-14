b6892be7bcf877ac6c3e1b4910382c6457f3f694
Karlo P <kpericic@student.unipu.hr>
2023-10-14 14:01:06 +0200

    Add top bar navigation

--- app/[id]/layout.tsx
+++ app/[id]/layout.tsx
@@ -1,10 +1,14 @@
 import SideBarNav from "@/src/components/navigation/SideBarNav";
+import TopBarNav from "@/src/components/navigation/TopBarNav";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="main-layout">
             <SideBarNav />
-            <section className="content">{children}</section>
+            <section className="content">
+                <TopBarNav />
+                {children}
+            </section>
         </main>
     );
 };
---
+++ src/components/navigation/TopBarNav.tsx
@@ -0,0 +1,9 @@
+const TopBarNav = () => {
+    return (
+        <div className="top-bar-nav">
+            <h1>Top bar nav</h1>
+        </div>
+    );
+};
+
+export default TopBarNav;
