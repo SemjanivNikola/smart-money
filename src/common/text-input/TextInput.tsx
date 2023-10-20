a074b64ac0d32d18b889e990260b7cd91d53f604
Karlo P <kpericic@student.unipu.hr>
2023-10-20 14:17:34 +0200

    Format code

--- src/common/text-input/TextInput.tsx
+++ src/common/text-input/TextInput.tsx
@@ -2,31 +2,29 @@ import { forwardRef, useRef } from "react";
 import TextInputProps from "./TextInputProps";
 import style from "./text-input.module.css";
 
-const TextInput = forwardRef<HTMLInputElement | null, TextInputProps>(
-    function TextInput(
-        { label, isFocused = false, error, ...otherProps },
-        ref
-    ) {
-        const inputRef = ref ? (ref as unknown as HTMLInputElement) : null;
-        const input = useRef<HTMLInputElement>(inputRef);
+const TextInput = forwardRef<HTMLInputElement | null, TextInputProps>(function TextInput(
+    { label, isFocused = false, error, ...otherProps },
+    ref
+) {
+    const inputRef = ref ? (ref as unknown as HTMLInputElement) : null;
+    const input = useRef<HTMLInputElement>(inputRef);
 
-        return (
-            <div className="relative mb-s text-start">
-                <div className="relative">
-                    <input autoFocus={isFocused} ref={input} {...otherProps} />
-                    <label
-                        id={`${otherProps.name}-label`}
-                        htmlFor={otherProps.name}
-                        className={`${style.input_label} ${style.idle} absolute px-s`}
-                        style={{ transform: "translateY(-50%)" }}
-                    >
-                        {label}
-                    </label>
-                </div>
-                <p className={style.input_error}>{error}</p>
+    return (
+        <div className="relative mb-s text-start">
+            <div className="relative">
+                <input autoFocus={isFocused} ref={input} className={style.input} {...otherProps} />
+                <label
+                    id={`${otherProps.name}-label`}
+                    htmlFor={otherProps.name}
+                    className={`${style.inputLabel} ${style.idle} absolute px-s`}
+                    style={{ transform: "translateY(-50%)" }}
+                >
+                    {label}
+                </label>
             </div>
-        );
-    }
-);
+            <p className={style.inputError}>{error}</p>
+        </div>
+    );
+});
 
 export default TextInput;
