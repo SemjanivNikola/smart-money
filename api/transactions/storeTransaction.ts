28795c213b80ec0db4a4ee3292bc1bc621c91362
SemjanivNikola <nikola.sem450@gmail.com>
2024-10-15 21:28:21 +0200

    Add catch method to chain fetch

--- api/transactions/storeTransaction.ts
+++ api/transactions/storeTransaction.ts
@@ -2,9 +2,13 @@ const URL = "http://127.0.0.1:8080";
 
 export async function storeTransaction(formData: any): Promise<any> {
     const response = await fetch(`${URL}/transactions`, {
-        method: "POST",
-        body: formData,
-    }).then((res) => res.json());
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .catch((error: Error) => {
+        throw error;
+      });
 
     return response;
 }
