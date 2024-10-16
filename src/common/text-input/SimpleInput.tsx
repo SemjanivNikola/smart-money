6999707d8be986c2870e2f5cda34a7202c5b9096
SemjanivNikola <nikola.sem450@gmail.com>
2024-10-16 16:59:10 +0200

    SimpleInput updated

--- src/common/text-input/SimpleInput.tsx
+++ src/common/text-input/SimpleInput.tsx
@@ -1,31 +1,27 @@
 "use client"
 
 import { forwardRef } from "react";
-import Icon from "../icon/Icon";
 import { SimpleInputProps } from "./TextInputProps";
 import s from "./text-input.module.css";
 
 const SimpleInput = forwardRef<HTMLInputElement | null, SimpleInputProps>(function TextInput(
-    { label, isFocused = false, error, icon, children, ...otherProps },
-    ref
+  { label, isFocused = false, error, helperText, children, ...otherProps },
+  ref
 ) {
-    return (
-        <div className={s.inputWrapper} data-error={!!error}>
-            <Icon name={icon} color="var(--black)" />
-            <input id={otherProps.name} ref={ref} autoFocus={isFocused} className={s.textInput} {...otherProps} />
-            {/* Label element needs to be next to input element so css selector can find it in order to animate */}
-            <label
-                id={`${otherProps.name}-label`}
-                htmlFor={otherProps.name}
-                className={s.inputLabel}
-                style={{ transform: "translateY(-50%)" }}
-            >
-                {label}
-            </label>
-            {children}
-            <span className={s.focusIndicator}></span>
-        </div>
-    );
+  const text = error ? error : helperText;
+  return (
+    <div className={s.inputWrapper} data-error={!!error}>
+      <label id={`${otherProps.name}-label`} htmlFor={otherProps.name} className={s.inputLabel}>
+        {label}
+      </label>
+      <div className={s.inputContainer}>
+        <input id={otherProps.name} ref={ref} autoFocus={isFocused} className={s.textInput} {...otherProps} />
+        {/* Label element needs to be next to input element so css selector can find it in order to animate */}
+        {children}
+      </div>
+      <span className={s.helperText}>{text}</span>
+    </div>
+  );
 });
 
 export default SimpleInput;
--- src/common/text-input/TextInputProps.ts
+++ src/common/text-input/TextInputProps.ts
@@ -2,18 +2,18 @@ import { ChangeEvent, InputHTMLAttributes } from "react";
 import { IconName } from "../icon/IconType";
 
 export type SimpleInputProps = {
-    name: string;
-    type: InputHTMLAttributes<HTMLInputElement>["type"];
-    value: InputHTMLAttributes<HTMLInputElement>["value"];
-    autoComplete?: InputHTMLAttributes<HTMLInputElement>["autoComplete"];
-    disabled?: boolean;
-    isFocused?: boolean;
-    error?: string;
-    label: string;
-    onBlur?: () => void;
-    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
-    icon: IconName;
-    children?: React.ReactElement;
+  name: string;
+  type: InputHTMLAttributes<HTMLInputElement>["type"];
+  value: InputHTMLAttributes<HTMLInputElement>["value"];
+  autoComplete?: InputHTMLAttributes<HTMLInputElement>["autoComplete"];
+  disabled?: boolean;
+  isFocused?: boolean;
+  error?: string;
+  helperText?: string;
+  label: string;
+  onBlur?: () => void;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  children?: React.ReactElement;
 };
 
 export type PasswordInputProps = Omit<SimpleInputProps, "type">;
