60b2ae48070f76ca2a7a6e4d73ae399a4261cb0a
SemjanivNikola <nikola.sem450@gmail.com>
2025-03-31 21:47:00 +0200

    Set default double content page component without divider and update width

--- src/layouts/DoubleContentPage.tsx
+++ src/layouts/DoubleContentPage.tsx
@@ -1,25 +1,52 @@
 import { ReactElement } from "react";
 import "./double-content.css";
 
+type DoubleContentPageProps = {
+  mainContent: ReactElement;
+  sideContent: ReactElement;
+  reverse?: boolean;
+  mainContentBasis?: number;
+  sideContentBasis?: number;
+};
+
 const DoubleContentPage = ({
   mainContent,
   sideContent,
   reverse = false,
-}: {
-  mainContent: ReactElement;
-  sideContent: ReactElement;
-  reverse?: boolean;
-}) => {
+  mainContentBasis = 50,
+  sideContentBasis = 50,
+}: DoubleContentPageProps) => {
+  let className = "double-content f a-s gap-m w-100 h-100";
+  if (reverse) className += " row-reverse";
+
+  return (
+    <div className={className}>
+      <div style={{ flexBasis: `${mainContentBasis}%` }}>{mainContent}</div>
+      <div style={{ flexBasis: `${sideContentBasis}%` }}>{sideContent}</div>
+    </div>
+  );
+};
+
+const DoubleContentWithDivider = ({
+  mainContent,
+  sideContent,
+  reverse = false,
+  mainContentBasis = 50,
+  sideContentBasis = 50,
+}: DoubleContentPageProps) => {
   let className = "double-content f a-s w-100";
   if (reverse) className += " row-reverse";
 
   return (
     <div className={className}>
-      {mainContent}
+      <div style={{ flexBasis: `${mainContentBasis}%` }}>{mainContent}</div>
       <div className="screen-content-divider" />
-      {sideContent}
+      <div style={{ flexBasis: `${sideContentBasis}%` }}>{sideContent}</div>
     </div>
   );
 };
+
+DoubleContentPage.Divider = DoubleContentWithDivider;
+
 
 export default DoubleContentPage;
