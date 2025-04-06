6ea5b1583aad974c95787baffb812fe88162f936
SemjanivNikola <nikola.sem450@gmail.com>
2025-04-06 16:07:43 +0200

    Create fetch transactions api

---
+++ api/transactions/fetchTransactios.ts
@@ -0,0 +1,39 @@
+import { PaymentTableRowProps } from "@/app/(main)/payments/PaymentRow";
+import { TableBodyItem } from "@/src/common/table/TableBody";
+import { TableHeaderItem } from "@/src/common/table/TableHeader";
+
+export async function fetchTransactions(query?: string): Promise<{
+  tableHeader: TableHeaderItem[];
+  tableData: TableBodyItem<PaymentTableRowProps>[];
+}> {
+  // const response = await fetch(`${process.env.SMART_MONEY_BE_URL}/transactions`, {
+  //   method: "GET",
+  // next: {
+  //   tags: ["fetch-transactions"];
+  // }
+  // })
+  //   .then((res) => res.json())
+  //   .catch((error: Error) => {
+  //     throw error;
+  //   });
+
+  const tableHeader: TableHeaderItem[] = [
+    { text: "Date", textPosition: "start", width: "12%" },
+    { text: "To/From", textPosition: "start" },
+    { text: "Amount", textPosition: "end" },
+    { text: "Status", width: "18%" },
+    { text: "" },
+  ];
+
+  const tableData = [
+    {
+      sectionTitle: "Yesterday",
+      items: [
+        { id: "1234", time: "10:29 PM", toFrom: "Name Middlename Surname", amount: 129.57, status: "Completed" },
+        { id: "1235", time: "10:30 PM", toFrom: "Name Middlename Surname1", amount: 130.57, status: "Pending" },
+      ],
+    },
+  ];
+
+  return { tableHeader, tableData };
+}
